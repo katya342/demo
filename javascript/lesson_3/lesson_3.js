f3e79e87d5cf02b627e989184439d1eb3c04c6fc
Katya <katya.bezverkhova228@mail.ru>
2022-07-23 10:43:12 +0600

    lesson_3

---
+++ javascript/lesson_3/lesson_3.js
@@ -0,0 +1,62 @@
+// const loop_count = parseInt(prompt("Кол-во повторений"));
+// let i = 0;
+// while
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+// const chislo = parseInt(prompt('Число для возведения в степень'));
+// const stepen = parseInt(prompt('Степень'));
+// let i = 0;
+// let total;
+// while (i<stepen)
+// {
+//     total = chislo * chislo;
+//         i++;
+// }
+// alert('Число  $(chislo)' + $(total));
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+// const first_number = prompt("Chislo 1: ");
+// const second_number = prompt("Chislo 2: ");
+// let i = 0;
+// let j = 0;
+//
+// let firstDivider = [];
+// let secondDivider = [];
+//
+// while(i < first_number)
+// {
+//    if(first_number % 1 === 0)
+//    {
+//        firstDivider.push(i);
+//    }
+//    i++;
+// }
+// while (j < second_number)
+// {
+//     if(second_number % j === 0)
+//     {
+//         secondDivider.push(j);
+//     }
+//     j++;
+// }
+// alert("Делители  числа 1: ${firstDivider.join()}" );
+// alert("Делители числа 2: " $(second_number));
+
+
+
+
+
+const userNumber = parseInt(prompt("Введите число"));
+let counter = 0;
+for(let i = 0; i<= userNumber; i++)
+{
+    
+}
+
+
+
+
+
+
